64a36a2d3a60ea354c5b550a9953991bd4d903f4
Momen <momenprogrammer@gmail.com>
2022-09-29 19:21:25 +0200

    Add Modal Box

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ const getElement = (selector) => {
 
 const mybutton = document.getElementById("scrollToUpButton");
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -17,16 +16,50 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
 const loader = document.querySelector(".pre-loader")
-
 window.addEventListener("load", vanish);
-
 function vanish() {
   loader.classList.add("disppear");
 }
+
+var modal_one = document.getElementById("myModalOne");
+var button_one = document.getElementById("myButtonOne");
+var span_one = document.getElementsByClassName("close-one")[0];
+button_one.onclick = function() {
+  modal_one.style.display = "block";
+}
+span_one.onclick = function() {
+  modal_one.style.display = "none";
+}
+
+var modal_two = document.getElementById("myModalTwo");
+var button_two = document.getElementById("myButtonTwo");
+var span_two = document.getElementsByClassName("close-two")[0];
+button_two.onclick = function() {
+  modal_two.style.display = "block";
+}
+span_two.onclick = function() {
+  modal_two.style.display = "none";
+}
+
+var modal_three = document.getElementById("myModalThree");
+var button_three = document.getElementById("myButtonThree");
+var span_three = document.getElementsByClassName("close-three")[0];
+button_three.onclick = function() {
+  modal_three.style.display = "block";
+}
+span_three.onclick = function() {
+  modal_three.style.display = "none";
+}
+
+const toggles = document.querySelectorAll('.q-and-a-toggle')
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
