fff03426c0d525568bc648481fa369cf6862495d
Momen <momenprogrammer@gmail.com>
2024-06-22 11:32:18 +0300

    Format main.js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const getElement = (selector) => {
-  const element = document.querySelector(selector)
-  
-  if (element) return element
+  const element = document.querySelector(selector);
+
+  if (element) return element;
   throw Error(
     `Please double check your class names, there is no ${selector} class`
-  )
-}
+  );
+};
 
 const mybutton = document.getElementById("scrollToUpButton");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -21,7 +23,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-const loader = document.querySelector(".pre-loader")
+const loader = document.querySelector(".pre-loader");
 window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
@@ -30,84 +32,92 @@ function vanish() {
 var modal_one = document.getElementById("myModalOne");
 var button_one = document.getElementById("myButtonOne");
 var span_one = document.getElementsByClassName("close-one")[0];
-button_one.onclick = function() {
+button_one.onclick = function () {
   modal_one.style.display = "block";
-}
-span_one.onclick = function() {
+};
+span_one.onclick = function () {
   modal_one.style.display = "none";
-}
+};
 
 var modal_two = document.getElementById("myModalTwo");
 var button_two = document.getElementById("myButtonTwo");
 var span_two = document.getElementsByClassName("close-two")[0];
-button_two.onclick = function() {
+button_two.onclick = function () {
   modal_two.style.display = "block";
-}
-span_two.onclick = function() {
+};
+span_two.onclick = function () {
   modal_two.style.display = "none";
-}
+};
 
 var modal_three = document.getElementById("myModalThree");
 var button_three = document.getElementById("myButtonThree");
 var span_three = document.getElementsByClassName("close-three")[0];
-button_three.onclick = function() {
+button_three.onclick = function () {
   modal_three.style.display = "block";
-}
-span_three.onclick = function() {
+};
+span_three.onclick = function () {
   modal_three.style.display = "none";
-}
+};
 
-const toggles = document.querySelectorAll('.q-and-a-toggle')
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
+const toggles = document.querySelectorAll(".q-and-a-toggle");
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
 
 let slideIndexOne = 1;
 showSlidesOne(slideIndexOne);
 function plusSlidesOne(n) {
-  showSlidesOne(slideIndexOne += n);
+  showSlidesOne((slideIndexOne += n));
 }
 function currentSlideOne(n) {
-  showSlidesOne(slideIndexOne = n);
+  showSlidesOne((slideIndexOne = n));
 }
 function showSlidesOne(n) {
   let i;
   let slides = document.getElementsByClassName("mySlidesOne");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndexOne = 1}    
-  if (n < 1) {slideIndexOne = slides.length}
+  if (n > slides.length) {
+    slideIndexOne = 1;
+  }
+  if (n < 1) {
+    slideIndexOne = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndexOne-1].style.display = "block";  
-  dots[slideIndexOne-1].className += " active";
+  slides[slideIndexOne - 1].style.display = "block";
+  dots[slideIndexOne - 1].className += " active";
 }
 
 let slideIndexTwo = 1;
 showSlidesTwo(slideIndexTwo);
 function plusSlidesTwo(n) {
-  showSlidesTwo(slideIndexTwo += n);
+  showSlidesTwo((slideIndexTwo += n));
 }
 function currentSlideTwo(n) {
-  showSlidesTwo(slideIndexTwo = n);
+  showSlidesTwo((slideIndexTwo = n));
 }
 function showSlidesTwo(n) {
   let i;
   let slides = document.getElementsByClassName("mySlidesTwo");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndexTwo = 1}    
-  if (n < 1) {slideIndexTwo = slides.length}
+  if (n > slides.length) {
+    slideIndexTwo = 1;
+  }
+  if (n < 1) {
+    slideIndexTwo = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndexTwo-1].style.display = "block";  
-  dots[slideIndexTwo-1].className += " active";
+  slides[slideIndexTwo - 1].style.display = "block";
+  dots[slideIndexTwo - 1].className += " active";
 }
